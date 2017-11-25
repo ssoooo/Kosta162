b9533b6bc4fa978fcd66520626b4dabe79cc07b3
yangheesoo <gmltn4515@naver.com>
2017-11-25 15:47:37 +0900

    for merge

---
+++ src/GeneralAffairs/service/MemberGroupService.java
@@ -0,0 +1,36 @@
+package GeneralAffairs.service;
+
+import java.util.List;
+
+import GeneralAffairs.domain.Group;
+import GeneralAffairs.domain.Member;
+
+public interface MemberGroupService {
+	
+	void createMember(Member member);
+	boolean modifyMember(Member member);
+	void removeMember(String memberId);
+	Member findMemberById(String memberId);
+	List<Member> findAllMembersByGroup(int groupId);
+	List<Member> findAllMembersByEvent(int eventId);
+	List<Member> findAllSignInGroupReq(int groupId);
+	void tradeGrade(String memberId,int groupId);
+	void reqSignInGroup(String memberId,int groupId);
+	void denySignInGroupReq(String memberId,int groupId);
+	
+	void createGroup(Group group);
+	boolean createMemberToGroup(String memberId,int groupId,String grade);
+	boolean modifyGroup(Group group);
+	void removeGroup(int groupId);
+	List<Group> findAllMyGroups(String memberId);
+	Group findGroupById(int groupId);
+	List<Group> findAllGroupsByMemberId(String memberId);
+	List<Group> findAllGroupsByGroupName(String groupName);
+	List<Group> findMyInvitationsByMemberId(String memberId);
+	void removeMemberFromGroup(String memberId,int groupId);
+	void acceptInvite(String memberId,int groupId);
+	void denyInvite(String memberId,int groupId);
+	boolean modifyGroupBalance(Group group,int price);
+	
+
+}
---
+++ src/GeneralAffairs/service/logic/MemberGroupServiceLogic.java
@@ -0,0 +1,151 @@
+package GeneralAffairs.service.logic;
+
+import java.util.List;
+
+import GeneralAffairs.domain.Group;
+import GeneralAffairs.domain.Member;
+import GeneralAffairs.service.MemberGroupService;
+
+public class MemberGroupServiceLogic implements MemberGroupService{
+
+	@Override
+	public void createMember(Member member) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean modifyMember(Member member) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeMember(String memberId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Member findMemberById(String memberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> findAllMembersByGroup(int groupId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> findAllMembersByEvent(int eventId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Member> findAllSignInGroupReq(int groupId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void tradeGrade(String memberId, int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reqSignInGroup(String memberId, int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void denySignInGroupReq(String memberId, int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void createGroup(Group group) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean createMemberToGroup(String memberId, int groupId, String grade) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modifyGroup(Group group) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeGroup(int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Group> findAllMyGroups(String memberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Group findGroupById(int groupId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Group> findAllGroupsByMemberId(String memberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Group> findAllGroupsByGroupName(String groupName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Group> findMyInvitationsByMemberId(String memberId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeMemberFromGroup(String memberId, int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void acceptInvite(String memberId, int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void denyInvite(String memberId, int groupId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean modifyGroupBalance(Group group, int price) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+
+}
